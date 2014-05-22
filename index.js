7636b0e1e5049b4b41877aff0bc02135b8d5f45f
Jason Trill <jason@jasontrill.com>
2014-05-22 03:45:44 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 hashwords = require('./lib/hashwords'); 
-if(typeof window != null)
-  window.hashwords = hashwords
+if(typeof window !== 'undefined')
+  window.hashwords = hashwords;
 else
-  module.exports = hashwords
+  module.exports = hashwords;
